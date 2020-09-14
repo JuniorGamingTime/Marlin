95c819c07be9e0a4840f72255a3faa1acc1d5e02
JuniorGamingTime <JuniorGamingTIme@gmail.com>
2020-09-14 11:30:59 -0400

    back to 16 micro stepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -746,7 +746,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 400, 400, 400, 144.46 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 400, 144.46 }
 
 /**
  * Default Max Feed Rate (mm/s)
