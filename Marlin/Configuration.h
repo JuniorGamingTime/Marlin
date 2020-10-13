6ce5d3361944e69a4bcf4f0e6862516f483a0a10
JuniorGamingTime <JuniorGamingTIme@gmail.com>
2020-10-13 16:57:43 -0400

    no changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -746,7 +746,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]144.46 3:1 393.55
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 400, 393.55 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 400, 400, 400, 393.55 }
 
 /**
  * Default Max Feed Rate (mm/s)
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2213,9 +2213,9 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       700        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       900        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     16    // 0..256
+    #define X_MICROSTEPS     32    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
@@ -2229,9 +2229,9 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       700
+    #define Y_CURRENT       900
     #define Y_CURRENT_HOME  Y_CURRENT
-    #define Y_MICROSTEPS     16
+    #define Y_MICROSTEPS     32
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
   #endif
